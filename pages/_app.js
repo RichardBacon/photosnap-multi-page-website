c4a97b4b214c27f95ca7d6e00c006bd57aa3a66b
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-08 19:02:18 +0100

    Add initial base styling
    - Install sass and configure stylelint for sass
    
    - Add reset, global and base styling
    
    - Install custom fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import '@fontsource/dm-sans/400.css';
+import '@fontsource/dm-sans/700.css';
+import '../styles/main.scss';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

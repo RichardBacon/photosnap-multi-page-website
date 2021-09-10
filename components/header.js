8d5f141747e2f1a95790a9d977e4f78d076bf198
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-10 14:27:30 +0100

    Make header responsive

--- components/header.js
+++ components/header.js
@@ -1,12 +1,41 @@
+import Link from 'next/link';
+import styles from '../styles/header.module.scss';
 import Logo from '../public/images/shared/logo.svg';
 import MenuIcon from '../public/images/shared/menu.svg';
-import styles from '../styles/header.module.scss';
+import Button from './button';
 
 function Header() {
   return (
     <header className={styles.header}>
       <Logo className={styles.logo} />
-      <MenuIcon />
+
+      <div className={styles.menuIconWrapper}>
+        <MenuIcon />
+      </div>
+
+      <nav className={styles.nav}>
+        <ul className={styles.navLinks}>
+          <li>
+            <Link href="/stories">
+              <a className={styles.navLink}>Stories</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/features">
+              <a className={styles.navLink}>Features</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/pricing">
+              <a className={styles.navLink}>Pricing</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.buttonWrapper}>
+        <Button caption="Get An Invite" style={1} />
+      </div>
     </header>
   );
 }

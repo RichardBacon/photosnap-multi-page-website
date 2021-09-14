7593c178292e038777f94129d7c0cbd15c5267d2
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-14 15:31:52 +0100

    Add hero to pricing page

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,11 +1,31 @@
+import Image from 'next/image';
 import Seo from '../components/seo';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 
 function Pricing() {
   return (
     <>
       <Seo />
-      <Layout />
+      <Layout>
+        <Hero
+          hero={{
+            heading: 'Pricing',
+            description:
+              'Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.',
+            image: (
+              <Image
+                src="/images/pricing/pricing1660x980.jpg"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                alt="A silouette of a woman using a camera on a tripod"
+              />
+            ),
+          }}
+          style={1}
+        />
+      </Layout>
     </>
   );
 }

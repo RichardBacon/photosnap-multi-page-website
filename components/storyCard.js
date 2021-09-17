b0ab38b6b0eedad2e2409e974f5b74613529f92a
Richard Bacon <richardjohnbacon@gmail.com>
2021-09-17 14:27:51 +0100

    Make whole story card link and add hover effect

--- components/storyCard.js
+++ components/storyCard.js
@@ -1,20 +1,27 @@
+import Link from 'next/link';
 import styles from '../styles/storyCard.module.scss';
-import Button from './button';
+import Arrow from '../public/images/shared/arrow.svg';
 
 function StoryCard({ story }) {
   return (
-    <figure className={styles.storyCard}>
-      <div className={styles.imageWrapper}>{story.image}</div>
+    <Link href="/">
+      <a className={styles.storyLink}>
+        <figure className={styles.storyCard}>
+          <div className={styles.imageWrapper}>{story.image}</div>
 
-      <div className={styles.content}>
-        <figcaption className={styles.figcaption}>
-          <h3 className={styles.heading}>{story.heading}</h3>
-          <h4 className={styles.author}>{`by ${story.author}`}</h4>
-        </figcaption>
-
-        <Button caption="Read Story" style={4} />
-      </div>
-    </figure>
+          <div className={styles.content}>
+            <figcaption className={styles.figcaption}>
+              <h3 className={styles.heading}>{story.heading}</h3>
+              <h4 className={styles.author}>{`by ${story.author}`}</h4>
+            </figcaption>
+            <div className={styles.button}>
+              Read Story
+              <Arrow className={styles.arrow} />
+            </div>
+          </div>
+        </figure>
+      </a>
+    </Link>
   );
 }
 
